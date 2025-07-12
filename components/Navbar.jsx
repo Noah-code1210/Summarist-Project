8af6db1ab7e79d454e10cf55f2c33b736d66c0d4
Noah <noahjames1035@gmail.com>
2025-07-12 13:02:05 -0400

    Navbar UI

---
+++ components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import Logo from "../assets/WebsiteLogo.png";
+
+function Navbar() {
+  return (
+    <>
+      <div id="navbar">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            <Image src={Logo} width={200} height={200} alt="Website Logo Img" className="logo-img" />
+            <ul className="flex gap-6 mr-18">
+              <li className="list">Login</li>
+              <li className="list">About</li>
+              <li className="list">Contact</li>
+              <li className="list">Help</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
