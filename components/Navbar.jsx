b02f945b290eeaba184f309d4a28a7c7135658c3
Noah <noahjames1035@gmail.com>
2025-07-13 15:15:16 -0400

    Header UI

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,17 +6,15 @@ function Navbar() {
   return (
     <>
       <div id="navbar">
-        <div className="container">
           <div className="flex justify-between items-center">
             <Image src={Logo} width={200} height={200} alt="Website Logo Img" className="logo-img" />
-            <ul className="flex gap-6 mr-18">
+            <ul className="flex gap-6 mr-[472px]">
               <li className="list">Login</li>
               <li className="list">About</li>
               <li className="list">Contact</li>
               <li className="list">Help</li>
             </ul>
           </div>
-        </div>
       </div>
     </>
   );
