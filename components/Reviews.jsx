4a7fc3b3879f4a798abef2b1373d7a36c490d6f7
Noah <noahjames1035@gmail.com>
2025-07-14 16:48:58 -0400

    Reviews section started

---
+++ components/Reviews.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Reviews() {
+  return (
+   <>
+   <div id="reviews">
+    <div className="container">
+        
+    </div>
+   </div>
+   </>
+  )
+}
+
+export default Reviews
