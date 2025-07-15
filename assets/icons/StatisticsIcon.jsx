4537beba63381674dace34a6ecd9921ec13edce6
Noah <noahjames1035@gmail.com>
2025-07-15 14:02:56 -0400

    statistics UI

--- assets/icons/StatisticsIcon.jsx
+++ assets/icons/StatisticsIcon.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { FaRegStarHalfStroke } from "react-icons/fa6";
+import { MdOutlineStarPurple500 } from "react-icons/md";
 
 function StatisticsIcon() {
   return (
-    <>
-    
-    </>
-  )
+    <div className="flex">
+      <MdOutlineStarPurple500 className="w-8 h-8" />{" "}
+      <MdOutlineStarPurple500 className="w-8 h-8" />{" "}
+      <MdOutlineStarPurple500 className="w-8 h-8" />{" "}
+      <MdOutlineStarPurple500 className="w-8 h-8" />{" "}
+      <FaRegStarHalfStroke className="w-[27px] h-[27px] mt-0.5" />
+    </div>
+  );
 }
 
-export default StatisticsIcon
+export default StatisticsIcon;
--- components/Statistics.jsx
+++ components/Statistics.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React from "react";
+import { FaCrown } from "react-icons/fa6";
+import { FaLeaf } from "react-icons/fa";
+import StatisticsIcon from "../assets/icons/StatisticsIcon"
 
 function Statistics() {
   return (
     <>
-    <div id="statistics">
-        <div className="container">
-            <div className="section-title">
-                Start growing with Summarist now
+      <div id="statistics">
+        <div className="container flex-col">
+          <div className="section-title">Start growing with Summarist now</div>
+          <div className="flex gap-x-10">
+            <div className="statistics__box">
+              <div >
+                <FaCrown className="statistics__logo"/>
+              </div>
+              <div className="statistics__title">3 Million</div>
+              <div className="statistics__para">Download on all platforms</div>
             </div>
             <div className="statistics__box">
-                
+              <div >
+                <StatisticsIcon/>
+              </div>
+              <div className="statistics__title">4.5 Stars</div>
+              <div className="statistics__para">
+                Average ratings on iOS and Google Play
+              </div>
             </div>
+            <div className="statistics__box">
+              <div >
+                <FaLeaf className="statistics__logo"/>
+              </div>
+              <div className="statistics__title">97%</div>
+              <div className="statistics__para">
+                Of Summarist members create a better reading habit
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Statistics
+export default Statistics;
