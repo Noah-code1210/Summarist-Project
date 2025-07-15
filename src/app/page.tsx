c65a6a1ad2c5920a5bf263c1f13dbe516eb16150
Noah <noahjames1035@gmail.com>
2025-07-15 14:28:16 -0400

    Main Ui finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header";
 import Info from "../../components/Info";
 import Reviews from "../../components/Reviews";
 import Statistics from "../../components/Statistics";
+import Footer from "../../components/Footer"
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Info />
       <Reviews />
       <Statistics />
+      <Footer />
     </>
   );
 }
