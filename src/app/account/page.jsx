e8452232b2d6b999088679fed3d72d7da2a1e391
Noah <noahjames1035@gmail.com>
2025-07-17 14:20:45 -0400

    ver navbar finished

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,11 +1,16 @@
+'use client'
 import Image from "next/image";
 import Logo from "../../../assets/WebsiteLogo.png";
 import { AiOutlineHome } from "react-icons/ai";
 import { BsBookmark } from "react-icons/bs";
 import { RiBallPenLine } from "react-icons/ri";
 import { IoMdSearch } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FaRegCircleQuestion } from "react-icons/fa6";
+import { MdLogout } from "react-icons/md";
 
 function page() {
+
   return (
     <section>
       <div className="fixed h-full w-full max-w-[200px] bg-[#f8f8f8] top-0 left-0 z-1000">
@@ -18,7 +23,7 @@ function page() {
             className="w-40 h-10"
           />
         </div>
-        <div className="flex-grow flex-shrink basis-[0%] mt-10">
+        <div className="flex-grow flex-shrink basis-[0%] mt-10 min-h-[615px]">
           <div>
             <a href="/account" className="account__tabs">
               <div>
@@ -26,25 +31,45 @@ function page() {
               </div>
               <div>For you</div>
             </a>
-            <a href="/account" className="account__tabs">
+            <a href="/library" className="account__tabs">
               <div>
                 <BsBookmark className="account__tab--img" />
               </div>
               <div>My Library</div>
             </a>
-            <a href="/account" className="account__tabs">
+            <a href="" className="account__tabs cursor-not-allowed hover:bg-transparent" >
               <div>
                 <RiBallPenLine className="account__tab--img w-6 h-6" />
               </div>
               <div>Highlights</div>
             </a>
-            <a href="/account" className="account__tabs">
+            <a href="" className="account__tabs cursor-not-allowed hover:bg-transparent" >
               <div>
                 <IoMdSearch className="account__tab--img" />
               </div>
               <div>Search</div>
             </a>
           </div>
+        </div>
+        <div>
+          <a href="/settings" className="account__tabs">
+              <div>
+                <IoSettingsOutline className="account__tab--img" />
+              </div>
+              <div>Settings</div>
+            </a>
+          <a href="" className="account__tabs cursor-not-allowed hover:bg-transparent">
+              <div>
+                <FaRegCircleQuestion className="account__tab--img"/>
+              </div>
+              <div>Help & Support</div>
+            </a>
+          <a href="" className="account__tabs">
+              <div>
+                <MdLogout className="account__tab--img" />
+              </div>
+              <div>Logout</div>
+            </a>
         </div>
       </div>
     </section>
