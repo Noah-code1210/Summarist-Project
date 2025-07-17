414f22fbd5387244e265ab6c4ff451053e389608
Noah <noahjames1035@gmail.com>
2025-07-17 15:01:10 -0400

    searchbar added

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Logo from "../../../assets/WebsiteLogo.png";
 import { AiOutlineHome } from "react-icons/ai";
@@ -10,66 +10,99 @@ import { FaRegCircleQuestion } from "react-icons/fa6";
 import { MdLogout } from "react-icons/md";
 
 function page() {
-
   return (
     <section>
-      <div className="fixed h-full w-full max-w-[200px] bg-[#f8f8f8] top-0 left-0 z-1000">
-        <div className="flex items-center justify-center h-15 pt-4 max-w-40 m-auto">
-          <Image
-            src={Logo}
-            width={200}
-            height={200}
-            alt="Website Logo Img"
-            className="w-40 h-10"
-          />
-        </div>
-        <div className="flex-grow flex-shrink basis-[0%] mt-10 min-h-[615px]">
-          <div>
-            <a href="/account" className="account__tabs">
-              <div>
-                <AiOutlineHome className="account__tab--img" />
+      <div className="relative flex flex-col ml-[200px] w-[1669px] transition-all">
+        <div className="bg-white border-b-1 border-solid border-b-[#e1e7ea] h-20 z-1">
+          <div className="relative flex items-center justify-between px-8 max-w-[1920px] mx-auto h-full">
+            <figure>
+              <img src="" alt="" />
+            </figure>
+            <div className="flex items-center gap-6 w-full max-w-[340px]">
+              <div className="flex items-center w-full">
+                <div className="relative gap-2">
+                  <input
+                    type="text"
+                    placeholder="Search for books"
+                    className="w-full h-10 px-4 outline-none bg-[#f1f6f4] text-[#042330] border-solid border-2 border-[#e1e7ea] rounded-lg"
+                  />
+                  <div className="flex items-center absolute h-full right-2 top-0 justify-end border-solid border-l-2 border-l-[#e1e7ea] pl-2">
+                    <div>
+                      <IoMdSearch className="w-7 h-7 text-[#03314b]"/>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div>For you</div>
-            </a>
-            <a href="/library" className="account__tabs">
-              <div>
-                <BsBookmark className="account__tab--img" />
-              </div>
-              <div>My Library</div>
-            </a>
-            <a href="" className="account__tabs cursor-not-allowed hover:bg-transparent" >
-              <div>
-                <RiBallPenLine className="account__tab--img w-6 h-6" />
-              </div>
-              <div>Highlights</div>
-            </a>
-            <a href="" className="account__tabs cursor-not-allowed hover:bg-transparent" >
-              <div>
-                <IoMdSearch className="account__tab--img" />
-              </div>
-              <div>Search</div>
-            </a>
+            </div>
           </div>
         </div>
-        <div>
-          <a href="/settings" className="account__tabs">
+        <div className="fixed h-full w-full max-w-[200px] bg-[#f8f8f8] top-0 left-0 z-1000">
+          <div className="flex items-center justify-center h-15 pt-4 max-w-40 m-auto">
+            <Image
+              src={Logo}
+              width={200}
+              height={200}
+              alt="Website Logo Img"
+              className="w-40 h-10"
+            />
+          </div>
+          <div className="flex-grow flex-shrink basis-[0%] mt-10 min-h-[615px]">
+            <div>
+              <a href="/account" className="account__tabs">
+                <div>
+                  <AiOutlineHome className="account__tab--img" />
+                </div>
+                <div>For you</div>
+              </a>
+              <a href="/library" className="account__tabs">
+                <div>
+                  <BsBookmark className="account__tab--img" />
+                </div>
+                <div>My Library</div>
+              </a>
+              <a
+                href=""
+                className="account__tabs cursor-not-allowed hover:bg-transparent"
+              >
+                <div>
+                  <RiBallPenLine className="account__tab--img w-6 h-6" />
+                </div>
+                <div>Highlights</div>
+              </a>
+              <a
+                href=""
+                className="account__tabs cursor-not-allowed hover:bg-transparent"
+              >
+                <div>
+                  <IoMdSearch className="account__tab--img" />
+                </div>
+                <div>Search</div>
+              </a>
+            </div>
+          </div>
+          <div>
+            <a href="/settings" className="account__tabs">
               <div>
                 <IoSettingsOutline className="account__tab--img" />
               </div>
               <div>Settings</div>
             </a>
-          <a href="" className="account__tabs cursor-not-allowed hover:bg-transparent">
+            <a
+              href=""
+              className="account__tabs cursor-not-allowed hover:bg-transparent"
+            >
               <div>
-                <FaRegCircleQuestion className="account__tab--img"/>
+                <FaRegCircleQuestion className="account__tab--img" />
               </div>
               <div>Help & Support</div>
             </a>
-          <a href="" className="account__tabs">
+            <a href="" className="account__tabs">
               <div>
                 <MdLogout className="account__tab--img" />
               </div>
               <div>Logout</div>
             </a>
+          </div>
         </div>
       </div>
     </section>
