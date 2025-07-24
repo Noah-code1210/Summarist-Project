e747d851958aa1506fb6bbb8eba8790b5f1fb66c
Noah <noahjames1035@gmail.com>
2025-07-24 12:32:46 -0400

    sidebar layout

--- src/app/(sidebar)/account/page.jsx
+++ src/app/(sidebar)/account/page.jsx
@@ -1,18 +1,11 @@
 "use client";
-import Image from "next/image";
-import Logo from "../../../assets/WebsiteLogo.png";
-import { AiOutlineHome } from "react-icons/ai";
-import { BsBookmark } from "react-icons/bs";
-import { RiBallPenLine } from "react-icons/ri";
 import { IoMdSearch } from "react-icons/io";
-import { IoSettingsOutline } from "react-icons/io5";
-import { FaRegCircleQuestion } from "react-icons/fa6";
-import { MdLogout } from "react-icons/md";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { IoIosStarOutline } from "react-icons/io";
-import SkeletonLoading from "../../../assets/LoadingState/SkeletonLoading";
-import Link from "next/link";
+import SkeletonLoading from "../../../../assets/LoadingState/SkeletonLoading";
+import Logo from "../../../../assets/WebsiteLogo.png";
+
 
 function page() {
   const [forYouBook, setForYouBook] = useState([]);
@@ -86,75 +79,7 @@ function page() {
               </div>
             </div>
           </div>
-          <div className="fixed h-full w-full max-w-[200px] bg-[#f8f8f8] top-0 left-0 z-1000">
-            <div className="flex items-center justify-center h-15 pt-4 max-w-40 m-auto">
-              <Image
-                src={Logo}
-                width={200}
-                height={200}
-                alt="Website Logo Img"
-                className="w-40 h-10"
-              />
-            </div>
-            <div className="flex-grow flex-shrink basis-[0%] mt-10 min-h-[615px]">
-              <div>
-                <a href="/account" className="account__tabs">
-                  <div>
-                    <AiOutlineHome className="account__tab--img" />
-                  </div>
-                  <div>For you</div>
-                </a>
-                <a href="/library" className="account__tabs">
-                  <div>
-                    <BsBookmark className="account__tab--img" />
-                  </div>
-                  <div>My Library</div>
-                </a>
-                <a
-                  href=""
-                  className="account__tabs cursor-not-allowed hover:bg-transparent"
-                >
-                  <div>
-                    <RiBallPenLine className="account__tab--img w-6 h-6" />
-                  </div>
-                  <div>Highlights</div>
-                </a>
-                <a
-                  href=""
-                  className="account__tabs cursor-not-allowed hover:bg-transparent"
-                >
-                  <div>
-                    <IoMdSearch className="account__tab--img" />
-                  </div>
-                  <div>Search</div>
-                </a>
-              </div>
-            </div>
-            <div>
-              <a href="/settings" className="account__tabs">
-                <div>
-                  <IoSettingsOutline className="account__tab--img" />
-                </div>
-                <div>Settings</div>
-              </a>
-              <a
-                href=""
-                className="account__tabs cursor-not-allowed hover:bg-transparent"
-              >
-                <div>
-                  <FaRegCircleQuestion className="account__tab--img" />
-                </div>
-                <div>Help & Support</div>
-              </a>
-              
-              <a href="/" className="account__tabs">
-                <div>
-                  <MdLogout className="account__tab--img" />
-                </div>
-                <div>Logout</div>
-              </a>
-            </div>
-          </div>
+          
           {loading ? (
             <>
               <SkeletonLoading />
--- src/app/Vertnavbar.jsx
+++ src/app/Vertnavbar.jsx
@@ -1,15 +1,16 @@
-"use client";
-import Image from "next/image";
-import Logo from "../../../assets/WebsiteLogo.png";
 import { AiOutlineHome } from "react-icons/ai";
 import { BsBookmark } from "react-icons/bs";
 import { RiBallPenLine } from "react-icons/ri";
-import { IoMdSearch } from "react-icons/io";
+import Image from "next/image";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaRegCircleQuestion } from "react-icons/fa6";
 import { MdLogout } from "react-icons/md";
+import Logo from "../../assets/WebsiteLogo.png";
+import { IoMdSearch } from "react-icons/io";
 
-function page() {
+
+
+function Vertnavbar() {
   return (
     <>
       <div className="fixed h-full w-full max-w-[200px] bg-[#f8f8f8] top-0 left-0 z-1000">
@@ -72,7 +73,7 @@ function page() {
             </div>
             <div>Help & Support</div>
           </a>
-          <a href="" className="account__tabs">
+          <a href="/" className="account__tabs">
             <div>
               <MdLogout className="account__tab--img" />
             </div>
@@ -80,8 +81,9 @@ function page() {
           </a>
         </div>
       </div>
+      ;
     </>
   );
 }
 
-export default page;
+export default Vertnavbar;
