e6d4aee8a9c59c087210afa208e4b1fb6c584db0
Noah <noahjames1035@gmail.com>
2025-07-25 16:08:53 -0400

    saved books title

--- src/app/(sidebar)/library/page.jsx
+++ src/app/(sidebar)/library/page.jsx
@@ -1,13 +1,12 @@
 "use client";
 
-
 function page() {
   return (
     <>
-      <div className="row">
-        <div className="container">
-          <div className="account__section--title">
-            Saved Books
+      <div className="relative flex flex-col ml-[200px] w-[1669px] transition-all">
+        <div className="w-full max-w-[1070px] mx-auto px-6">
+          <div className="w-full py-10">
+            <div className="account__section--title">Saved Books</div>
           </div>
         </div>
       </div>
