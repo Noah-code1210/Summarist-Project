87706e235ea19aecce304d49e7ea651660b0b6c4
Noah <noahjames1035@gmail.com>
2025-07-27 12:12:01 -0400

    Choose-plan started

---
+++ src/app/choose-plan/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function choosePlan() {
+  return (
+    <>
+    <div className='w-full'>
+        <div className='relative text-center w-full pt-12 mb-6 before:content-[""] before:absolute before:top-0 before:left-0 before:z-[-1] before:w-full before:h-full before:bg-[#032b41] rounded-b-[16rem]'>
+          
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default choosePlan
